253b7b6d4215dcffcd843715cf1becb781dae2a3
nileshshirkemet <43514175+nileshshirkemet@users.noreply.github.com>
2025-04-11 11:21:53 +0530

    Add files via upload

--- CodingSamples/DotNet/Fundamentals/Basics/InheritanceTest/ClassTypeTest/DemoApp2/Interval.cs
+++ CodingSamples/DotNet/Fundamentals/Basics/InheritanceTest/ClassTypeTest/DemoApp2/Interval.cs
@@ -1,7 +1,7 @@
 //primary constructor
 class Interval(int min, int sec)
 {
-    //read-only properties
+    //read-only properties (no set block)
     public int Minutes { get; } = min + sec / 60;
 
     public int Seconds { get; } = sec % 60;
@@ -15,5 +15,37 @@ class Interval(int min, int sec)
     public static Interval operator+(Interval lhs, Interval rhs)
     {
         return new Interval(lhs.Minutes + rhs.Minutes, lhs.Seconds + rhs.Seconds);
+    }
+
+    //overriding corresponding method of System.Object to return
+    //a string containing values of fields in this instance
+    public override string ToString()
+    {
+        if(Seconds >= 10)
+            return Minutes + ":" + Seconds;
+        return Minutes + ":0" + Seconds;
+    }
+
+    //overriding corresponding method of System object to return
+    //an integer obtained by combining values of fields in this
+    //instance such that the same integer is returned for two
+    //instances with matching field values
+    public override int GetHashCode()
+    {
+        return Minutes + Seconds;
+    }
+
+    //overriding corresponding method of System object to return
+    //true or false depending on whether or not the values of fields
+    //in this instance match with those in instance referred by 
+    //the argument passed 
+    public override bool Equals(object other)
+    {
+        if(other is Interval)
+        {
+            Interval that = (Interval)other; //explicit down-casting
+            return this.Minutes == that.Minutes && this.Seconds == that.Seconds;
+        }
+        return false;
     }
 }
--- CodingSamples/DotNet/Fundamentals/Basics/InheritanceTest/ClassTypeTest/DemoApp2/Program.cs
+++ CodingSamples/DotNet/Fundamentals/Basics/InheritanceTest/ClassTypeTest/DemoApp2/Program.cs
@@ -1,10 +1,30 @@
-class Program
+//C# supports top level statements in one source file
+//of a project with OutputType as Exe, these statements
+//are copied at compile time into the Main method of
+//an auto-generated Program class
+Console.WriteLine("Welcome User");
+Interval a = new Interval(6, 50);
+Print("Interval a", a);
+Interval b = new Interval(4, 15);
+Print("Interval b", b);
+Print("Sum", a + b);
+Interval c = new Interval(5, 110);
+Print("Interval c", c);
+Interval d = b;
+Print("Interval d", d);
+Console.WriteLine("-------------------------------");
+Console.WriteLine("a is identical to b: {0}", ReferenceEquals(a, b));
+Console.WriteLine("a is identical to c: {0}", ReferenceEquals(a, c));
+Console.WriteLine("d is identical to b: {0}", ReferenceEquals(d, b));
+Console.WriteLine("-------------------------------");
+Console.WriteLine("a is equal to b: {0}", Equals(a, b));
+Console.WriteLine("a is equal to c: {0}", Equals(a, c));
+Console.WriteLine("d is equal to b: {0}", Equals(d, b));
+
+
+//a local function (included withing Main method), such
+//functions do not support overloading
+void Print(string label, object info)
 {
-    static void Main()
-    {
-        Interval a = new Interval(4, 50);
-        Interval b = new Interval(3, 15);
-        Interval c = a + b;
-        
-    }
+    Console.WriteLine("{0} = {1}", label, info);
 }
