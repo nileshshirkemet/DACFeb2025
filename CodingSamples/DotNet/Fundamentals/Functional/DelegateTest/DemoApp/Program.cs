43525f40f3afbc7b51da61b6715a56faade3f45b
nileshshirkemet <43514175+nileshshirkemet@users.noreply.github.com>
2025-04-17 12:08:09 +0530

    Add files via upload

---
+++ CodingSamples/DotNet/Fundamentals/Functional/DelegateTest/DemoApp/Program.cs
@@ -0,0 +1,19 @@
+﻿namespace DemoApp;
+
+class Program
+{
+    static double SafeScheme(int period)
+    {
+        return period < 3 ? 5.5 : 6;
+    }
+
+    static void Main(string[] args)
+    {
+        double p = double.Parse(args[0]);
+        int n = int.Parse(args[1]);
+        var a = new Investment(p, n);
+        Console.WriteLine("Future value for riskless investment: {0:0.00}", a.FutureValue(SafeScheme));
+        //passing lambda-expression: (arguments) => body
+        Console.WriteLine("Future value for riskful investment : {0:0.00}", a.FutureValue(y => 8 + 0.25 * y));
+    }
+}
---
+++ CodingSamples/DotNet/Fundamentals/Functional/LinqTest/DemoApp/Program.cs
@@ -0,0 +1,10 @@
+﻿using DemoApp;
+
+if (args[0] == "items")
+{
+    var selection = Shop.GetItems()
+        .Where(i => i.Brand == args[1])
+        .Select(i => i.Name.ToUpper());
+    foreach(var entry in selection)
+        Console.WriteLine(entry);
+}
