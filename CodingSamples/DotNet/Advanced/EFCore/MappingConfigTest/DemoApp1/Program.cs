a6daf95a69a6a2a0b298f972b6f4bad39bc2979a
nileshshirkemet <43514175+nileshshirkemet@users.noreply.github.com>
2025-04-25 17:35:03 +0530

    Add files via upload

---
+++ CodingSamples/DotNet/Advanced/EFCore/MappingConfigTest/DemoApp1/Program.cs
@@ -0,0 +1,23 @@
+﻿using DemoApp.Shopping.Data;
+using Microsoft.EntityFrameworkCore;
+
+using var shop = new ShopDbContext();
+if(args.Length == 0)
+{
+    foreach(var customer in shop.Customers)
+        Console.WriteLine("{0}\t{1:0.00}", customer.Id, customer.Credit);
+}
+else
+{
+    var customer = shop.Customers
+        .Where(c => c.Id == args[0])
+        .Include(c => c.Orders) //eager loading of child entities
+        .FirstOrDefault();
+    if(customer != null)
+    {
+        foreach(var order in customer.Orders)
+            Console.WriteLine("{0}\t{1}\t{2:yyyy-MM-dd}", order.ProductId, order.Quantity, order.OrderDate);
+    }
+    else
+        Console.WriteLine("No such customer!");
+}
